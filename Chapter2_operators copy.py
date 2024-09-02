23c7be4435d6f9ffbe6dab3e0c2819b290625989
Talha Farooq Khan <96606224+TFKOrig@users.noreply.github.com>
2024-09-02 10:30:45 +0500

    Add files via upload

---
+++ Chapter2_operators copy.py
@@ -0,0 +1,13 @@
+print(2+3)      #Addition 
+print(2-3)      #Subtraction
+print(2*3)      #Multiplication   
+print(7/2)      #Division 
+print(2**3)     #Power Operator used to add power 
+print(2%3)      #Modulus gives remainder 
+print(7//2)     #Floor Division  gives nearest whole number
+
+
+print(3**2/2*3/3+6-4)
+#PEMDAS         parantheis,exponential,multiplication,division,addition,subtraction
+
+#MD and AS      left to right
---
+++ Chapter3_strings.py
@@ -0,0 +1,26 @@
+# It can be done using single '' double "" and triple ''' ''' quotes
+
+print('Hello World1')
+print("Hello World2")
+print('''Hello World3''')
+
+#MultiLine strings      
+
+#1. using single triple quotes
+
+x= '''I'm learning python for Artificial Intelligence,machine learning,deep learning,data analysis and for data scientist'''
+
+print(x)
+
+#2. using double triple quotes
+
+x= """I'm learning python for Artificial Intelligence,machine learning,deep learning,data analysis and for data scientist"""
+
+print(x)
+
+
+#strings are arrays
+
+a="hello,world"
+
+print(a[0])
